f910b4485fdc304c454e3d289190c3cbffd1ba99
Damien Cauquil <dcauquil@quarkslab.com>
2021-10-27 21:21:51 +0200

    Improved the proto_send_ack() function and added declaration of CMD_ERR.

--- proto.c
+++ proto.c
@@ -3,32 +3,6 @@
 
 extern process_event_t kb_sendpkt_message;
 
-void proto_send_ack(command_t command)
-{
-  kb_event_t *p_kb_evt;
-  uint8_t checksum;
-
-  /* Allocate memory for kb_event_t structure. */
-  p_kb_evt = (kb_event_t *)malloc(sizeof(kb_event_t));
-  if (p_kb_evt != NULL)
-  {
-    /* Allocate memory for our payload. */
-    p_kb_evt->payload = (uint8_t *)malloc(3);
-    if (p_kb_evt->payload != NULL)
-    {
-      /* Build our event. */
-      p_kb_evt->payload[0] = 3;
-      p_kb_evt->payload[1] = command;
-      checksum = packet_compute_checksum(p_kb_evt->payload, 2);
-      p_kb_evt->payload[2] = checksum;
-      p_kb_evt->payload_size = 3;
-
-      /* Send packet to USB process. */
-      process_post(PROCESS_BROADCAST, kb_sendpkt_message, (void *)p_kb_evt);
-    }
-  }
-}
-
 void proto_send_packet(packet_t *p_pkt)
 {
   kb_event_t *p_kb_evt;
@@ -59,6 +33,37 @@ void proto_send_packet(packet_t *p_pkt)
 
       /* Free packet. */
       free(p_pkt);
+    }
+  }
+}
+
+void proto_send(command_t command, uint8_t *payload, int len)
+{
+  kb_event_t *p_kb_evt;
+  int i;
+  uint8_t checksum;
+
+  /* Allocate memory for kb_event_t structure. */
+  p_kb_evt = (kb_event_t *)malloc(sizeof(kb_event_t));
+  if (p_kb_evt != NULL)
+  {
+    /* Allocate memory for our payload. */
+    p_kb_evt->payload = (uint8_t *)malloc(len + 3);
+    if (p_kb_evt->payload != NULL)
+    {
+      /* Build our event. */
+      p_kb_evt->payload[0] = (uint8_t)(len + 3);
+      p_kb_evt->payload[1] = command;
+      for (i=0; i<len; i++)
+      {
+        p_kb_evt->payload[2+i] = payload[i];
+      }
+      checksum = packet_compute_checksum(p_kb_evt->payload, len+2);
+      p_kb_evt->payload[2+i] = checksum;
+      p_kb_evt->payload_size = len + 3;
+
+      /* Send packet to USB process. */
+      process_post(PROCESS_BROADCAST, kb_sendpkt_message, (void *)p_kb_evt);
     }
   }
 }
--- proto.h
+++ proto.h
@@ -24,7 +24,8 @@ typedef enum {
   CMD_SNIFF_ON_ACK,
   CMD_SNIFF_OFF,
   CMD_SNIFF_OFF_ACK,
-  CMD_GOT_PKT
+  CMD_GOT_PKT,
+  CMD_ERR = 0xFF
 } command_t;
 
 typedef struct {
@@ -32,8 +33,11 @@ typedef struct {
   uint8_t payload[256];
 } packet_t;
 
-void proto_send_ack(command_t command);
+//void proto_send_ack(command_t command);
 //void proto_send_packet(uint8_t *p_packet, int packet_len);
 void proto_send_packet(packet_t *p_pkt);
+void proto_send(command_t command, uint8_t *payload, int len);
+
+#define proto_send_ack(x) proto_send(x, NULL, 0)
 
 #endif /* __INC_SERIAL_PROTO_H */
